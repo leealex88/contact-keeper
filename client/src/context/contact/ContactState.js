62d4e89fa064e474dfea613ab624347c7728ac87
ola <olasvoice@gmail.com>
2020-10-19 11:42:52 +0200

    context
    context

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,43 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CURRENT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    constacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jill@gmail.com",
+        phone: "111-111-111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Sara Watson",
+        email: "sarah@gmail.com",
+        phone: "222-222-222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Harry White",
+        email: "harry@gmail.com",
+        phone: "333-333-333",
+        type: "personal",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState)
+};
